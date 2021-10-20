a6bb31e6fffe426c62f36ac9c67db45e474cd761
cry-daniel <1522914980@qq.com>
2021-10-21 00:08:08 +0800

    HOG pre_work finish

--- HOG+SVM/test.py
+++ HOG+SVM/test.py
@@ -2,18 +2,34 @@ import utils
 import configs
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 pos=utils.read_pos_route()
 
 tot=0
 
 for route in pos:
-    tot+=1
     img=utils.read_img(route)
+    plt.subplot(2,2,1)
+    plt.title('origin')
+    plt.imshow(img)
     #print(np.size(img,0),'x',np.size(img,1),'x',np.size(img,2))
-    Img=cv2.filter2D(img,-1,configs.kernel_x)
-    Img=cv2.filter2D(Img,-1,configs.kernel_y)
-    if tot<4:
-        print(np.size(Img,0),'x',np.size(Img,1),'x',np.size(Img,2))
-        cv2.imshow('x',Img)
-        cv2.waitKey(0)
+    Img_y=cv2.filter2D(img,cv2.CV_16S,configs.kernel_y)
+    plt.subplot(2,2,2)
+    plt.title('y position')
+    plt.imshow(Img_y)
+    Img_x=cv2.filter2D(img,cv2.CV_16S,configs.kernel_x)
+    plt.subplot(2,2,3)
+    plt.title('x position')
+    plt.imshow(Img_x)
+    Img=abs(Img_x)+abs(Img_y)
+    plt.subplot(2,2,4)
+    plt.title('x * y')
+    plt.imshow(Img)
+    plt.show()
+    arc_max,Img_max=utils.cal_arctan(Img,Img_x,Img_y)
+    print(arc_max)
+    res=utils.histogram(Img_max,arc_max,0,0)
+    print(res)
+    input()
+    
--- HOG+SVM/utils.py
+++ HOG+SVM/utils.py
@@ -2,6 +2,9 @@ import configs
 import cv2
 import numpy as np
 
+c_x=configs.cell_x
+c_y=configs.cell_y
+
 def read_pos_route():
     pos=[]
     fp=open(configs.pos_route,'r')
@@ -21,3 +24,43 @@ def read_neg_route():
 def read_img(route):
     img=cv2.imread('./INRIAPerson/'+route)
     return img
+
+def cal_arctan(Img,Img_x,Img_y):
+    length=np.size(Img,0)
+    width=np.size(Img,1)
+    temp=np.zeros((length,width))
+    temp_img=np.zeros((length,width))
+    for i in range(0,length):
+        for j in range(0,width):
+            maxx=-1
+            flag=-1
+            for k in range(0,3):
+                if Img[i][j][k]>maxx:
+                    maxx=Img[i][j][k]
+                    flag=k
+            temp_img[i][j]=Img[i][j][flag]
+            if Img_x[i][j][flag]==0:
+                if Img_y[i][j][flag]==0:
+                    temp[i][j]=0
+                else:
+                    temp[i][j]=90
+                    
+            elif Img_y[i][j][flag]==0:
+                if temp[i][j]<0:
+                    temp[i][j]=0
+                else:
+                    temp[i][j]=180
+            else:
+                temp[i][j]=abs(np.arctan2(Img_x[i][j][flag],Img_y[i][j][flag])*180/np.pi)        
+    return temp,temp_img
+
+def histogram(Img_max,arc_max,pos_x,pos_y):
+    length=np.size(Img_max,0)
+    width=np.size(Img_max,1)
+    res=np.zeros(10)
+    for i in range(pos_x*c_x,(pos_x+1)*c_x):
+        for j in range(pos_y*c_y,(pos_y+1)*c_y):
+            minn=int(arc_max[i][j]/20)
+            res[minn]+=(20*(minn+1)-arc_max[i][j])/20*Img_max[i][j]
+            res[min(minn+1,9)]+=(arc_max[i][j]-20*minn)/20*Img_max[i][j]
+    return res
